aac06d7d97e285431d2308c64bc9e9fe1e37bb65
skfamim251 <shaikfamim@gmail.com>
2025-07-07 01:50:46 +0530

    Update app.py

--- calorie/app.py
+++ calorie/app.py
@@ -1,1 +1,27 @@
+from flask import Flask, request, jsonify
+import pickle
+import numpy as np
+
+app = Flask(__name__)
+
+# Load the trained model
+with open('calorie_predictor.pkl', 'rb') as f:
+    model = pickle.load(f)
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.get_json()
+    steps = data.get('steps')
+    duration = data.get('duration')
+    weight = data.get('weight')
+
+    # Model expects 2D array
+    prediction = model.predict([[steps, duration, weight]])[0]
+
+    return jsonify({
+        'predicted_calories': round(prediction, 2)
+    })
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
